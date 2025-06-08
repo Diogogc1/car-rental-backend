f15fdde7327bc218b2723d5632f468d74535ab80
Diogogc1 <diogogomescastro@outlook.com>
2025-06-07 23:33:12 -0300

    fix: Add password and remove unnecessary id in constructor

--- backend/src/entities/user.entity.ts
+++ backend/src/entities/user.entity.ts
@@ -1,9 +1,11 @@
 export class User {
   name: string;
   email: string;
+  password: string;
 
-  constructor(id: string, name: string, email: string) {
+  constructor(name: string, email: string, password: string) {
     this.name = name;
     this.email = email;
+    this.password = password;
   }
 }
