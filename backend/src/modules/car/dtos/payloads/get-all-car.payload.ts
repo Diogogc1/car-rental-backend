2c14c4d60f86b1bfdbaf219d0be561ba6f90fd3c
Diogogc1 <diogogomescastro@outlook.com>
2025-06-21 20:54:22 -0300

    Refactor: Change Brazilian for Mercosul

--- backend/src/modules/car/dtos/payloads/get-all-car.payload.ts
+++ backend/src/modules/car/dtos/payloads/get-all-car.payload.ts
@@ -29,7 +29,7 @@ export class GetAllCarPayload
   })
   @IsOptional()
   @Matches(/^[A-Z]{3}\d[A-Z]\d{2}$/, {
-    message: 'Plate must follow the Brazilian format ABC1D23',
+    message: 'Plate must follow the Mercosul format ABC1D23',
   })
   @IsString()
   plate?: string;
