4d1216a422d3c3ab14aa16e7ae5068170c4e3935
Diogogc1 <diogogomescastro@outlook.com>
2025-06-23 10:07:04 -0300

    feat: Login screen

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -9,46 +9,20 @@ export default function Home() {
   const router = useRouter();
 
   useEffect(() => {
-    if (status === "unauthenticated") {
-      router.push("/api/auth/signin");
+    if (status === "loading") return;
+
+    if (!session) {
+      router.push("/login");
     }
-  }, [status, router]);
+  }, [session, status, router]);
 
   if (status === "loading") {
-    return (
-      <div className="flex h-screen items-center justify-center">
-        <p>Carregando...</p>
-      </div>
-    );
+    return <div>Carregando...</div>;
   }
 
   if (!session) {
-    return null;
+    return <div>Redirecionando...</div>;
   }
-  return (
-    <div className="flex h-screen w-full">
-      <div className="flex-1 bg-primary"></div>
-      <div className="flex-1/12 bg-gray-200 flex-col items-center justify-center">
-        <h1 className="text-center">Login</h1>
-        <form className="flex flex-col items-center">
-          <input
-            type="text"
-            placeholder="Username"
-            className="mb-4 p-2 border border-gray-300 rounded"
-          />
-          <input
-            type="password"
-            placeholder="Password"
-            className="mb-4 p-2 border border-gray-300 rounded"
-          />
-          <button
-            type="submit"
-            className="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600"
-          >
-            Login
-          </button>
-        </form>
-      </div>
-    </div>
-  );
+
+  return <div>Bem-vindo, {session.user?.email}!</div>;
 }
